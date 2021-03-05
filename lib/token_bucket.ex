ecb07cd4426bd43e63c9fc3167975ce46c83b742
Zach Blankenship <zach.blankenship@gmail.com>
2021-03-05 15:55:17 -0800

    Initial commit

---
+++ lib/token_bucket.ex
@@ -0,0 +1,18 @@
+defmodule TokenBucket do
+  @moduledoc """
+  Documentation for `TokenBucket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TokenBucket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
